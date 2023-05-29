d52ad95a078a48485229dc0c9db96bda1a856760
Fernando Ramos Timote <fernando.ramos@smartquick.com.co>
2023-05-29 11:26:09 -0500

    ajustes

--- back/app/Http/Controllers/RouteController.php
+++ back/app/Http/Controllers/RouteController.php
@@ -77,15 +77,6 @@ class RouteController extends Controller
         foreach ($data->data as $item) {
             $order = Order::select("contact", "identification","phone", "email", 'zip_code')->where("id", $item->order_id)->first();
             $stop = Stops::select("photo_delivery","photo_cancellation","comment_cancellation","comment_delivery")->where("order_id", $item->order_id)->first();
-
-            $stop->img = '';
-            if($stop->photo_delivery){
-                $stop->img = Storage::url($stop->photo_delivery);
-            }else{
-                if($stop->photo_cancellation){
-                    $stop->img = Storage::url($stop->photo_cancellation);
-                }
-            }
             $item->order = $order;
             $item->stops = $stop;
         }
